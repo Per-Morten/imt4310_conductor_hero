be08784b69cbb29ecf4288b0be4e6dddfbcacc45
school-dev <school-dev>
2018-02-07 11:40:15 +0100

    ref: 26 Add preliminary sphere collision code

--- conductor_hero/Assets/Scripts/MotionTrackerSphere.cs
+++ conductor_hero/Assets/Scripts/MotionTrackerSphere.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MotionTrackerSphere : MonoBehaviour {
+public class MotionTrackerSphere : MonoBehaviour
+{
+    [SerializeField]
+    private Material m_defaultMaterial;
+    [SerializeField]
+    private Material m_onTriggerMaterial;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private int m_SphereIndex = 0;
+
+    private MeshRenderer m_meshRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_meshRenderer = GetComponent<MeshRenderer>();
+        m_meshRenderer.material = m_defaultMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_meshRenderer.material = m_onTriggerMaterial;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_meshRenderer.material = m_defaultMaterial;
+    }
 }
