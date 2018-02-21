51db7e21d9470666cac1165dac5880155ac89cf3
Per-Morten Straume <gamma_ray_per@hotmail.com>
2018-02-21 14:38:08 +0100

    ref #31: Start implementing Cue logic
    Started working on the cue logic, cna now click on the different spheres
    to mute and unmute the sound. Need to start looking into interpolation
    and countdowns.

---
+++ conductor_hero/Assets/Scripts/Cue.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cue
+    : MonoBehaviour
+{
+    public void Start()
+    {
+        GameObject.Find("Metronome").GetComponent<Metronome>().onBeatTickedCallback += new Metronome.OnBeatTickCallback(OnBeat);
+    }
+
+    public void ReInit(int countdown, AudioManager.Instrument trackToUnmute, AudioManager audioManager)
+    {
+        m_beatCountdown = countdown;
+        m_trackToUnmute = trackToUnmute;
+        m_audioManager = audioManager;
+
+        m_mode = Mode.countdown;
+    }
+
+    void Update()
+    {
+        if (m_mode == Mode.idle)
+            return;
+
+        if (m_mode == Mode.success)
+            PlaySuccessAnimation();
+
+        if (m_mode == Mode.failed)
+            PlayFailAnimation();
+
+        // Sort of doing two modes here really, going out of the board, up to the user, and displaying the countdown.
+        // if (m_mode == Mode.countdown)
+        //     DisplayCountdown();
+
+
+    }
+
+    public void PlaySuccessAnimation()
+    {
+
+    }
+
+    public void PlayFailAnimation()
+    {
+
+    }
+
+    public void OnBeat(int beatID)
+    {
+        m_beatCountdown--;
+        // Should we potentially also mute instrument here 1 beat before it starts, to ensure that the instrument isn't on when cued?
+    }
+
+    // Only relevant in editor
+    public void OnMouseDown()
+    {
+        ReactToHit();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        ReactToHit();
+    }
+
+    private void ReactToHit()
+    {
+        // Should use beatID in addition to countdown
+        // But need to wait until that functionality is there
+        if (m_beatCountdown >= -1 && m_beatCountdown <= 1)
+        {
+            Debug.LogFormat("You hit!");
+            Renderer r = gameObject.GetComponent<Renderer>();
+            r.material.color = new Color(1.0f, 0.0f, 0.0f);
+
+            m_audioManager.MuteInstrument(m_trackToUnmute, false);
+        }
+    }
+
+    enum Mode
+    {
+        idle,
+        rising, // Use 1 bar rising out from the table
+        countdown, // 1 bar counting down from 4 to 1. 
+        success, // Interpolate to the orchestra
+        failed, // Fall limp to the ground
+    };
+
+    AudioManager m_audioManager;
+    AudioManager.Instrument m_trackToUnmute;
+    int m_beatCountdown;
+    bool m_isHit;
+    Mode m_mode;
+
+    
+}
---
+++ conductor_hero/Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Instrument = AudioManager.Instrument;
+
+public class GameManager
+    : MonoBehaviour
+{ 
+    void Start()
+    {
+        // Create cue signal objects
+        // Setup which beats we need to push on
+        // These times are based on a temporary hack of starting the song 1 bar later, for testing
+        // Need to be readjusted.
+        m_sectionsToCueOnBeat = new Dictionary<int, Instrument>
+        {
+            { 5, Instrument.glock },
+            { 36, Instrument.harpsichord },
+            { 50, Instrument.violins_extra }
+        };
+
+        m_cueSignals = new List<Cue>
+        {
+            GameObject.Find("Sphere").GetComponent<Cue>(),
+            GameObject.Find("Sphere (1)").GetComponent<Cue>(),
+            GameObject.Find("Sphere (2)").GetComponent<Cue>(),
+            GameObject.Find("Sphere (3)").GetComponent<Cue>(),
+
+        };
+
+
+        m_metronome = GameObject.Find("Metronome").GetComponent<Metronome>();
+        m_metronome.onBeatTickedCallback += new Metronome.OnBeatTickCallback(OnBeat);
+
+        m_audioManager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+        m_audioManager.MuteInstrument(Instrument.glock, true);
+        m_audioManager.MuteInstrument(Instrument.harpsichord, true);
+        m_audioManager.MuteInstrument(Instrument.violins_extra, true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnBeat(int beatID)
+    {
+        Debug.LogFormat("BeatID{0}", beatID);
+        if (m_sectionsToCueOnBeat.ContainsKey(beatID))
+        {
+            //Debug.LogFormat("In if statement");
+            int index = (int)m_sectionsToCueOnBeat[beatID];
+
+            Renderer r = m_cueSignals[index].GetComponent<Renderer>();
+            r.material.color = new Color(0.5f, 0.5f, 1.0f);
+
+            // - 1 because we are on the beat where this will happen
+            m_cueSignals[index].ReInit(m_cueCountdown - 1, m_sectionsToCueOnBeat[beatID], m_audioManager);
+        }
+    }
+
+    // Responsibilities
+    // - Create the cues when needed 
+    // - Check if the cues are hit
+    //      - Check if they are hit on beat
+    //      - Mute and unmute tracks
+    // - 
+
+    [SerializeField]
+    List<Cue> m_cueSignals;
+
+    // Holds when they should start to be queued in
+    // In this case, 2 bars (i.e. 8 beats) before they are activated
+
+    [SerializeField]
+    Dictionary<int, Instrument> m_sectionsToCueOnBeat;
+
+    [SerializeField]
+    AudioManager m_audioManager;
+
+    [SerializeField] // How many beats before the actual beat do we notify the user?
+    int m_cueCountdown = 4;
+
+    Metronome m_metronome;
+}
