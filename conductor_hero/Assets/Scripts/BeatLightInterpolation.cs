77e638aca8c025d5502e0626062172c6d5739342
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2018-02-24 13:13:47 +0100

    Add fog, light to beat interpolation and updated level Added a budget fog effect(could be improved with animated fog textures and what not) as well as a beat synchronised interpolation of the stage light. Also added the updated level to the gesture testing scene.

---
+++ conductor_hero/Assets/Scripts/BeatLightInterpolation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatLightInterpolation : MonoBehaviour {
+    public Light lightSource;
+    public Metronome metronome;
+
+    [SerializeField]
+    private float m_fadeSpeed = 1;
+
+    [SerializeField]
+    private AnimationCurve m_fadeVelocity;
+
+    private float m_baseIntensity;
+    private float m_interpTimer = 0;
+
+    void Start()
+    {
+        m_baseIntensity = lightSource.intensity;
+        metronome.onBeatTickedCallback += OnBeatLight;
+    }
+
+    void Update()
+    {
+        m_interpTimer += m_fadeSpeed * Time.deltaTime;
+        lightSource.intensity = Mathf.Lerp(m_baseIntensity, 0, m_fadeVelocity.Evaluate(m_interpTimer));
+    }
+
+    private void OnBeatLight(int updatedBeatID)
+    {
+        m_interpTimer = 0;
+        lightSource.intensity = m_baseIntensity;
+    }
+
+
+}
